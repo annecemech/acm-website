9bdd548cead3dd378783ff8d3ca47ac38ed3d9cc
AnneCecile Mechin <ac.mechin@gmail.com>
2021-01-26 17:38:54 +0100

    add blue background on navbar when scrolling

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -11,3 +11,19 @@ function addActiveClass(e) {
     }
     e.currentTarget.classList.add('active');
 }
+
+// Add and remove .navbar-scroll class on Navbar when scrolling
+
+let scrollPos = window.scrollY;
+const navbar = document.querySelector(".my-navbar");
+const navbarHeight = navbar.offsetHeight
+
+const add_class_on_scroll = () => navbar.classList.add("navbar-scroll")
+const remove_class_on_scroll = () => navbar.classList.remove("navbar-scroll")
+
+window.addEventListener('scroll', function() {
+  scrollPos = window.scrollY;
+
+  if (scrollPos >= navbarHeight) { add_class_on_scroll() }
+  else { remove_class_on_scroll() }
+})
